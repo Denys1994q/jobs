c3caa5fcd30a6718c6b2731a0def5241fff7855d
Denys1994q <drybachok@gmail.com>
2022-11-11 18:09:25 +0200

    pageSlider ready start maps

--- pages/[id].js
+++ pages/[id].js
@@ -2,9 +2,11 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useHttp } from "../hooks/http.hook";
 import Image from "next/image";
+import Link from "next/link";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark, faShare, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark, faShareAlt, faChevronLeft, faLocationPin } from "@fortawesome/free-solid-svg-icons";
 
 // 404 сторінку створити
 //  спінера прикрутити з реакт спінерс
@@ -21,6 +23,8 @@ const Job = () => {
         setJob(arr.filter(item => item.id === id));
     };
 
+    // const formatSalary = job && job.length > 0 ?  : null;
+
     useEffect(() => {
         request(
             "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
@@ -29,21 +33,21 @@ const Job = () => {
 
     return (
         <>
-            {job ? (
+            {job && job.length > 0 ? (
                 <div className='job'>
-                    <div className='job-left'>
+                    <div className='job__left'>
                         <div className='job__top'>
                             <p className='job__top-title'>Job Details</p>
                             <FontAwesomeIcon icon={faBookmark} className='fa-regular'></FontAwesomeIcon>
                             <p className='job__top-iconText job__top-iconText-save'>Save to my list</p>
-                            <FontAwesomeIcon icon={faShare} className='fa-regular'></FontAwesomeIcon>
+                            <FontAwesomeIcon icon={faShareAlt} className='fa-regular'></FontAwesomeIcon>
                             <p className='job__top-iconText job__top-iconText-share'>Share</p>
                         </div>
                         <button className='btn job__top-btn'>Apply now</button>
                         <div className='job__title-box'>
                             <p className='job__title-box-left'>{job[0].title}</p>
                             <div className='job__title-box-right'>
-                                <p>€ {job[0].salary}</p>
+                                <p>€ {job[0].salary.replace(/[k]/g, " 000")}</p>
                                 <p>Brutto, per year</p>
                             </div>
                         </div>
@@ -81,18 +85,32 @@ const Job = () => {
                                 );
                             })}
                         </ul>
-                        <button className='btn__return'>
-                            <FontAwesomeIcon
-                                icon={faChevronLeft}
-                                className='fa-regular fa-2x btn__return-caret'></FontAwesomeIcon>
-                            <span>Return to job board</span>
-                        </button>
+                        <Link href='/'>
+                            <button className='btn__return'>
+                                <FontAwesomeIcon
+                                    icon={faChevronLeft}
+                                    className='fa-regular fa-2x btn__return-caret'></FontAwesomeIcon>
+                                <span>Return to job board</span>
+                            </button>
+                        </Link>
                     </div>
 
-                    <div className='job-right'>2</div>
+                    <div className='job__right'>
+                        <div className='job__right-infocard'>
+                            <p className='job__right-infocard-name'>{job[0].name}</p>
+                            <p className='job__right-infocard-address'>
+                                <FontAwesomeIcon icon={faLocationPin}></FontAwesomeIcon>
+                                <span>{job[0].address}</span>
+                            </p>
+                            <p className='job__right-infocard-phone'>{job[0].phone},</p>
+                            <p className='job__right-infocard-email'>{job[0].email}</p>
+                        </div>
+                    </div>
                 </div>
             ) : (
-                <p>Загрузка...</p>
+                <div className='loading'>
+                    <ClipLoader color={"#fd7d24"} size={15} />
+                </div>
             )}
         </>
     );
--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,128 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useHttp } from "../hooks/http.hook";
 import Link from "next/link";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faLocationPin, faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faLocationPin, faBookmark, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-// https://www.youtube.com/watch?v=9e-5QHpadi0 - гугл меп 
-// по ходу треба самому сторінки зробити знизу. Взяти масив з 20 робіт і поділити по 5.. у верстку засунути перші 5 і далі по кліку перерендерювати додаючи наступні +5 через функції роботи з масивом типу splice. Перший раз від 0 до 5, далі від 6 до 10, далі від 11 до 15...
-export default function Home() {
+// ГУГЛ КАРТИ
+// МОБІЛЬНУ ВЕРСІЮ 
+// ДЕПЛОЙ, ЧИ НОРМАЛЬНО ПРАЦЮЄ ЯКЩО ПРЯМО ОНОВИТИ СТОРІНКУ З РОБОТОЮ 
+
+// оскільки дані при кожному новому запиті приходять трохи інші, то обрав getServerSideProps, а не getStaticProps
+export async function getServerSideProps(context) {
     const { request } = useHttp();
 
-    const [jobs, setJobs] = useState([]);
+    const res = await request(
+        `https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu`
+    );
+    return {
+        props: { jobs: res },
+    };
+}
 
-    useEffect(() => {
-        request(
-            "https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"
-        ).then(data => setJobs(data));
-    }, []);
+// https://www.youtube.com/watch?v=9e-5QHpadi0 - гугл меп
+
+export default function Home({ jobs }) {
+    // лічильник для того, щоб показати не всі роботи відразу, а по кілька робіт на сторінці (встановив по 5 пропозицій робіт на одній сторінці)
+    const [jobStart, setJobStart] = useState(0);
 
     const showJobsList =
+        jobs.length > 0 ? (
+            jobs.slice(jobStart, jobStart + 5).map(item => {
+                return (
+                    <li className='jobsList__item'>
+                        <div className='jobsList__item-photo'>
+                            {/* <Image width={85} height={85} src={item.pictures[0]} /> */}
+                            <img src={item.pictures[2]} alt='job-photo' />
+                        </div>
+                        <div className='jobsList__item-textbox'>
+                            <div className='jobsList__item-textbox-title'>
+                                <Link href={`/${item.id}`}>{item.title}</Link>
+                            </div>
+                            <div className='jobsList__item-textbox-subtitle'>
+                                Department name • Allgemeines Krankenhaus der Stadt Wien - AKH
+                            </div>
+                            <div className='jobsList__item-textbox-place'>
+                                <FontAwesomeIcon icon={faLocationPin} className='fa-regular fa-lg'></FontAwesomeIcon>
+                                <p> Vienna, Austria</p>
+                            </div>
+                        </div>
+                        <div className='jobsList__item-raiting'>
+                            <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
+                            <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
+                            <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
+                            <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
+                            <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
+                        </div>
+                        <div className='jobsList__item-date'>
+                            <FontAwesomeIcon icon={faBookmark} className='jobsList__item-date-icon'></FontAwesomeIcon>
+                            <p>Posted 2 days ago</p>
+                        </div>
+                    </li>
+                );
+            })
+        ) : (
+            <div className='loading'>
+                <ClipLoader color={"#fd7d24"} size={15} />
+            </div>
+        );
+
+    // показуємо по 5 оголошень роботи на сторінці. Зараз у масив приходить з северу 20 робіт, тому створюється 4 li з цифрами.
+    const showPagesNumbers =
         jobs.length > 0
-            ? jobs.map(item => {
-                  return (
-                      <li className='jobsList__item'>
-                          <div className='jobsList__item-photo'>
-                              {/* <Image width={85} height={85} src={item.pictures[0]} /> */}
-                              <img src={item.pictures[2]} alt='job-photo' />
-                          </div>
-                          <div className='jobsList__item-textbox'>
-                              <div className='jobsList__item-textbox-title'>
-                                  <Link href={`/${item.id}`}>{item.title}</Link>
-                              </div>
-                              <div className='jobsList__item-textbox-subtitle'>
-                                  Department name • Allgemeines Krankenhaus der Stadt Wien - AKH
-                              </div>
-                              <div className='jobsList__item-textbox-place'>
-                                  <FontAwesomeIcon icon={faLocationPin} className='fa-regular fa-lg'></FontAwesomeIcon>
-                                  <p> Vienna, Austria</p>
-                              </div>
-                          </div>
-                          <div className='jobsList__item-raiting'>
-                              <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
-                              <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
-                              <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
-                              <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
-                              <FontAwesomeIcon icon={faStar} className='fa-regular'></FontAwesomeIcon>
-                          </div>
-                          <div className='jobsList__item-date'>
-                              <FontAwesomeIcon icon={faBookmark} className='jobsList__item-date-icon'></FontAwesomeIcon>
-                              <p>Posted 2 days ago</p>
-                          </div>
-                      </li>
-                  );
+            ? jobs.map((item, index) => {
+                  if (index < jobs.length / 5) {
+                      return (
+                          <li
+                              onClick={() => showChosenPage(index)}
+                              className={index === jobStart / 5 ? "activePageNumber" : null}>
+                              {index + 1}
+                          </li>
+                      );
+                  }
               })
             : null;
+
+    // показуємо наступну сторінку
+    const showNextPage = () => {
+        if (jobStart !== jobs.length - 5) {
+            setJobStart(old => old + 5);
+        }
+    };
+
+    // показуємо попередню сторінку
+    const showPrevPage = () => {
+        if (jobStart !== 0) {
+            setJobStart(old => old - 5);
+        }
+    };
+
+    // показуємо ту сторінку, по якій клікнув користувач
+    const showChosenPage = pageNumber => {
+        setJobStart(pageNumber * 5);
+    };
 
     return (
         <div>
             <div className='container'>
                 <ul className='jobsList'>{showJobsList}</ul>
+                <div className='pagesPanel'>
+                    <ul>
+                        <FontAwesomeIcon
+                            icon={faChevronLeft}
+                            style={{ color: "#7D859C", opacity: jobStart !== 0 ? "1" : ".2" }}
+                            onClick={() => showPrevPage()}
+                            className='fa-2x pagesPanel-leftArrow'></FontAwesomeIcon>
+                        {showPagesNumbers}
+                        <FontAwesomeIcon
+                            icon={faChevronRight}
+                            style={{ color: "#7D859C", opacity: jobStart !== jobs.length - 5 ? "1" : ".2" }}
+                            onClick={() => showNextPage()}
+                            className='fa-2x pagesPanel-rightArrow'></FontAwesomeIcon>
+                    </ul>
+                </div>
             </div>
         </div>
     );
